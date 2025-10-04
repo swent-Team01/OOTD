a6d075dd622597225f2549fe14a8c8ce2ce15964
asunluer <asliunluer276@gmail.com>
2025-10-04 23:41:27 +0200

    style(items): apply ktfmt formatting

--- app/src/main/java/com/android/ootd/model/Item.kt
+++ app/src/main/java/com/android/ootd/model/Item.kt
@@ -4,10 +4,10 @@ import android.net.Uri
 
 data class Item(
     val itemId: String,
-    val image : Uri,
+    val image: Uri,
     val category: String,
     val type: String,
-    val brand : String,
+    val brand: String,
     val price: Double,
     val material: String,
     val link: String,
