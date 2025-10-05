07b7a6524c0f43cc39ab3c560993cf2fb1432b16
Clemens <94059427+Clemensito@users.noreply.github.com>
2025-10-05 12:25:32 +0200

    add tests to increase code coverage
    tests were mandatory to pass 80%, they are nothing special so don't rely
    on them. They check that there is a main and second activity

---
+++ app/src/androidTest/java/com/android/ootd/GreetingPreviewTest.kt
@@ -0,0 +1,18 @@
+package com.android.ootd
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class GreetingPreviewTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun showsPreviewText() {
+        composeRule.setContent { GreetingPreview() }
+        composeRule.onNodeWithText("Preview").assertExists()
+    }
+}
