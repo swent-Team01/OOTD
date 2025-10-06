e7edf1e5c1f358b48f8e7e94e4ff496fe36a5417
mej <julien.meric@epfl.ch>
2025-10-06 20:23:03 +0200

    feat(ui): update color palette based on Figma

--- app/src/main/java/com/android/sample/ui/theme/Color.kt
+++ app/src/main/java/com/android/sample/ui/theme/Color.kt
@@ -2,9 +2,12 @@ package com.android.sample.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF6750A4)
-val Secondary = Color(0xFF625B71)
-val Tertiary = Color(0xFF7D5260)
-
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
+/**
+ * App color palette. Colors are sourced from the OOTD Figma design
+ * (https://www.figma.com/design/EQfCuEx3jJpUSZ3NKc4DE5/OOTD?node-id=3541-1522&t=uwN1pE45GerKiYfv-0)
+ */
+val Primary = Color(0xFF5167FC)
+val Secondary = Color(0xFFE9EBF8)
+val Tertiary = Color(0xFFB4B8C5)
+val Last = Color(0xFFD9D9D9)
+val Background = Color(0xFFFEFFFE)
