d64a0a03e53169d228634fa93ac323625c1c1899
Clemens <94059427+Clemensito@users.noreply.github.com>
2025-10-09 00:18:15 +0200

    fix(format): did not add after formatting last push
    ...

--- app/src/androidTest/java/com/android/ootd/screen/RegisterScreenTest.kt
+++ app/src/androidTest/java/com/android/ootd/screen/RegisterScreenTest.kt
@@ -15,96 +15,97 @@ import org.junit.Rule
 import org.junit.Test
 
 class RegisterScreenTest {
-    @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @Before
-    fun setUp() {
-        composeTestRule.setContent { RegisterScreen() }
+  @Before
+  fun setUp() {
+    composeTestRule.setContent { RegisterScreen() }
+  }
+
+  @Test
+  fun displayAllComponents() {
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE)
+        .assertTextContains("Save", substring = true, ignoreCase = true)
+    composeTestRule.onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(RegisterScreenTestTags.APP_LOGO).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(RegisterScreenTestTags.WELCOME_TITLE).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertIsNotDisplayed()
+  }
+
+  @Test
+  fun canEnterUsername() {
+    val text = "user1"
+    composeTestRule.enterUsername(text)
+
+    composeTestRule.waitForIdle()
+
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME)
+        .assertTextContains(text)
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertDoesNotExist()
+  }
+
+  @Test
+  fun noError_whenFieldNotTouched() {
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertDoesNotExist()
+  }
+
+  @Test
+  fun noError_whenTyping_beforeLeavingField() {
+    composeTestRule.onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME).performClick()
+    composeTestRule.enterUsername("validUser")
+
+    composeTestRule.waitForIdle()
+
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertDoesNotExist()
+  }
+
+  @Test
+  fun buttonVisible_whenNoErrors() {
+    composeTestRule.enterUsername("validUser")
+
+    composeTestRule.waitUntil(timeoutMillis = 3000) {
+      composeTestRule
+          .onAllNodesWithTag(RegisterScreenTestTags.REGISTER_SAVE)
+          .fetchSemanticsNodes()
+          .isNotEmpty()
     }
 
-    @Test
-    fun displayAllComponents() {
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE)
-            .assertTextContains("Save", substring = true, ignoreCase = true)
-        composeTestRule.onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME).assertIsDisplayed()
-        composeTestRule.onNodeWithTag(RegisterScreenTestTags.APP_LOGO).assertIsDisplayed()
-        composeTestRule.onNodeWithTag(RegisterScreenTestTags.WELCOME_TITLE).assertIsDisplayed()
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertIsNotDisplayed()
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertDoesNotExist()
+    composeTestRule.onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE).assertIsDisplayed()
+  }
+
+  @Test
+  fun buttonVisible_whenNoErrors_isEnabled() {
+    composeTestRule.enterUsername("validUser")
+
+    composeTestRule.waitUntil(timeoutMillis = 3000) {
+      val nodes =
+          composeTestRule
+              .onAllNodesWithTag(RegisterScreenTestTags.REGISTER_SAVE)
+              .fetchSemanticsNodes()
+      nodes.isNotEmpty()
     }
 
-    @Test
-    fun canEnterUsername() {
-        val text = "user1"
-        composeTestRule.enterUsername(text)
+    composeTestRule.waitForIdle()
 
-        composeTestRule.waitForIdle()
-
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME)
-            .assertTextContains(text)
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertDoesNotExist()
-    }
-
-    @Test
-    fun noError_whenFieldNotTouched() {
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertDoesNotExist()
-    }
-
-    @Test
-    fun noError_whenTyping_beforeLeavingField() {
-        composeTestRule.onNodeWithTag(RegisterScreenTestTags.INPUT_REGISTER_UNAME).performClick()
-        composeTestRule.enterUsername("validUser")
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertDoesNotExist()
-    }
-
-    @Test
-    fun buttonVisible_whenNoErrors() {
-        composeTestRule.enterUsername("validUser")
-
-        composeTestRule.waitUntil(timeoutMillis = 3000) {
-            composeTestRule
-                .onAllNodesWithTag(RegisterScreenTestTags.REGISTER_SAVE)
-                .fetchSemanticsNodes()
-                .isNotEmpty()
-        }
-
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertDoesNotExist()
-        composeTestRule.onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE).assertIsDisplayed()
-    }
-
-    @Test
-    fun buttonVisible_whenNoErrors_isEnabled() {
-        composeTestRule.enterUsername("validUser")
-
-        composeTestRule.waitUntil(timeoutMillis = 3000) {
-            val nodes = composeTestRule
-                .onAllNodesWithTag(RegisterScreenTestTags.REGISTER_SAVE)
-                .fetchSemanticsNodes()
-            nodes.isNotEmpty()
-        }
-
-        composeTestRule.waitForIdle()
-
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
-            .assertDoesNotExist()
-        composeTestRule
-            .onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE)
-            .assertIsDisplayed()
-            .assertIsEnabled()
-    }
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.ERROR_MESSAGE, useUnmergedTree = true)
+        .assertDoesNotExist()
+    composeTestRule
+        .onNodeWithTag(RegisterScreenTestTags.REGISTER_SAVE)
+        .assertIsDisplayed()
+        .assertIsEnabled()
+  }
 }
