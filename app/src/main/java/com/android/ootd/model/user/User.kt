f7dcff28b301aa26a5cc36b2a433e2da7d3d86bb
mej <julien.meric@epfl.ch>
2025-10-13 11:34:46 +0200

    feat(model): change profile picture type to Uri
    In the user class, the profilePicture attribute
    changed to Uri to match the rest of the app.

--- app/src/main/java/com/android/ootd/model/user/User.kt
+++ app/src/main/java/com/android/ootd/model/user/User.kt
@@ -1,16 +1,18 @@
 package com.android.ootd.model.user
 
+import android.net.Uri
+
 /**
  * Represents a friend of a user.
  *
  * @property uid Firebase user id of the friend.
  * @property name Display name of the friend.
- * @property profilePicture Firebase Storage download URL for the friend's profile picture.
+ * @property profilePicture Firebase Storage download URI for the friend's profile picture.
  */
 data class Friend(
     val uid: String = "",
     val name: String = "",
-    val profilePicture: String = "" // Firebase Storage download URL
+    val profilePicture: Uri = Uri.EMPTY // Firebase Storage download URL
 )
 
 /**
@@ -18,12 +20,12 @@ data class Friend(
  *
  * @property uid Firebase user id.
  * @property name Display name of the user.
- * @property profilePicture Firebase Storage download URL for the user's profile picture.
+ * @property profilePicture Firebase Storage download URI for the user's profile picture.
  * @property friendList List of the user's friends.
  */
 data class User(
     val uid: String = "",
     val name: String = "",
-    val profilePicture: String = "", // Firebase Storage download URL
+    val profilePicture: Uri = Uri.EMPTY, // Firebase Storage download URL
     val friendList: List<Friend> = emptyList()
 )
