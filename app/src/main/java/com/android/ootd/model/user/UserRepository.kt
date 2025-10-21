2aaa775eff60b2846e0dd13bdfe9e0b400d7abc4
mej <julien.meric@epfl.ch>
2025-10-21 20:09:56 +0200

    refractor(model): simplify User data class and update related tests
    Simplify user by reducing its attributes to uid and username for
    public access. Private access is now implemented by account. Use the new public User shape (uid and username) across repositories and tests. Remove friend-related fields, DTO entries, and repository APIs that referenced friend lists.

--- app/src/main/java/com/android/ootd/model/user/UserRepository.kt
+++ app/src/main/java/com/android/ootd/model/user/UserRepository.kt
@@ -44,30 +44,4 @@ interface UserRepository {
    * @throws Exception If the user is not found
    */
   suspend fun userExists(userID: String): Boolean
-
-  /**
-   * Adds the user with friendID in the friend list of userID.
-   *
-   * @param userID The unique identifier of the User which friend's list is updated.
-   * @param friendID The unique identifier of the User which will be added to the friend list.
-   * @throws Exception if userID or friendID is not found.
-   */
-  suspend fun addFriend(userID: String, friendID: String)
-
-  /**
-   * Removes the user with friendID from the friend list of userID.
-   *
-   * @param userID The unique identifier of the User which friend's list is updated.
-   * @param friendID The unique identifier of the User which will be removed to the friend list.
-   * @throws Exception if userID or friendID is not found
-   */
-  suspend fun removeFriend(userID: String, friendID: String)
-  /**
-   * Checks if for userID, the user with id friendID is his friend.
-   *
-   * @param userID The unique identifier of the user which friend list is checked.
-   * @param friendID The unique identifier of the user looked for in the friend list.
-   * @throws Exception if friendID is not found or user is not authenticated
-   */
-  suspend fun isMyFriend(userID: String, friendID: String): Boolean
 }
