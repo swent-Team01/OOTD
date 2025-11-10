bd51aec1664e08381777d4a1895596e1e603f3ca
Stefan Taga <stefan.taga@epfl.ch>
2025-11-10 09:04:11 +0100

    fix(theme): Remove useless code

--- app/src/main/java/com/android/ootd/ui/theme/Theme.kt
+++ app/src/main/java/com/android/ootd/ui/theme/Theme.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -36,7 +35,6 @@ fun OOTDTheme(
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
   }
