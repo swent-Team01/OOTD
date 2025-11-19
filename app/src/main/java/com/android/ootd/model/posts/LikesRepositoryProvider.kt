ca3b30e41caf3a768c0d1aed4c19e01b40ebcd6b
bbianca2004 <biancapitu11@gmail.com>
2025-11-19 05:29:37 +0100

    feat(model): add repository provider for likes

--- app/src/main/java/com/android/ootd/model/posts/LikesRepositoryProvider.kt
+++ app/src/main/java/com/android/ootd/model/posts/LikesRepositoryProvider.kt
@@ -1,4 +1,12 @@
 package com.android.ootd.model.posts
 
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
 object LikesRepositoryProvider {
+  private val _repository: LikesRepository by lazy {
+    LikesFirestoreRepository(db = Firebase.firestore)
+  }
+
+  var repository: LikesRepository = _repository
 }
