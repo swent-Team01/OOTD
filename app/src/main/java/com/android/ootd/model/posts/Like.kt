f61bf6e44c047aeb42ebd4cb948c0629b83bc807
bbianca2004 <biancapitu11@gmail.com>
2025-12-17 13:48:29 +0100

    feat(doc): add kdoc for like data class

--- app/src/main/java/com/android/ootd/model/posts/Like.kt
+++ app/src/main/java/com/android/ootd/model/posts/Like.kt
@@ -1,3 +1,13 @@
 package com.android.ootd.model.posts
 
+import androidx.annotation.Keep
+
+/**
+ * Represents a like on a post
+ *
+ * @property postId ID of the liked post
+ * @property postLikerId UID of the user who liked the post
+ * @property timestamp time passed since liking
+ */
+@Keep
 data class Like(val postId: String = "", val postLikerId: String = "", val timestamp: Long = 0L)
